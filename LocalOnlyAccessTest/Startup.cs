904500600f80ba1655a248d0755b788ddfbb9dee
Sander Kooij <sanderkooij@xs4all.nl>
2018-06-15 01:11:19 +0200

    + Add project files

---
+++ LocalOnlyAccessTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocalOnlyAccessTest.Startup))]
+namespace LocalOnlyAccessTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
